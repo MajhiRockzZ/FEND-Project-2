918eb2ea760cdc182974072ded0d25c20eec5985
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-08-07 09:36:38 +0530

    Add game timer

--- js/app.js
+++ js/app.js
@@ -135,3 +135,21 @@ function moveCounter(){
         }
     }
 }
+
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute + "mins " + second + "secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
+}
